466b557a19ca411d74f9109bf35da684b228b097
Konstantin Safronov <konstsaf@gmail.com>
2019-07-01 16:48:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ config.debug = process.env.DEBUG || false;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '/#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '192.168.1.1'; //'m20.cloudmqtt.com';
-config.mqtt.port      = process.env.MQTT_PORT      || 1883; // 10852;
-config.mqtt.username  = process.env.MQTT_USERNAME  || "box01"; //"cedprdzm";
-config.mqtt.password  = process.env.MQTT_PASSWORD  || "1380bis"; //"r4z4KoSZrCrk";
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '192.168.1.1'; //
+config.mqtt.port      = process.env.MQTT_PORT      || 1883; // 
+config.mqtt.username  = process.env.MQTT_USERNAME  || "box01"; //
+config.mqtt.password  = process.env.MQTT_PASSWORD  || "1380bis"; //
 
 config.db = {};
 config.db.hostname  = 'localhost';
